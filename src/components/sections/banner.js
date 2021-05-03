18ee3e29a97fc17941306b06d6578b4a662239e2
vamshi krishna <vamshi.kr777@gmail.com>
2021-05-03 09:46:21 +0530

    Stopped tracking .env File

--- src/components/sections/banner.js
+++ src/components/sections/banner.js
@@ -5,7 +5,7 @@ import '../../app.css';
 const Banner = (props) => {
     const {backgroundImg} = props;
     return (
-            <section className="banner"
+            <header className="banner"
             style = {{ backgroundImage: 
                 `linear-gradient(to bottom , rgba(0, 0, 0, 0) 39%, rgba(0, 0, 0, 0.1) 41%, rgba(0, 0, 0, 0.6) 75%, rgba(0, 0, 0, 1) 100%),
                  url('${backgroundImg}')`
@@ -23,7 +23,7 @@ const Banner = (props) => {
                         <button><Link to="/about">Know More</Link></button>
                     </div>
                 </div>
-            </section>
+            </header>
     )
 }
 
